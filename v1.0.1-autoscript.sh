0b8616d37cad54b2d380d220a4efbc7a5d35feee
hazennetworksolutions <161881320+hazennetworksolutions@users.noreply.github.com>
2025-05-16 22:33:15 +0300

    Update v1.0.1-autoscript.sh

--- v1.0.1-autoscript.sh
+++ v1.0.1-autoscript.sh
@@ -1,1 +1,251 @@
+#!/bin/bash
+LOG_FILE="/var/log/lumera_node_install.log"
+exec > >(tee -a "$LOG_FILE") 2>&1
 
+printGreen() {
+    echo -e "\033[32m$1\033[0m"
+}
+
+printLine() {
+    echo "------------------------------"
+}
+
+# Function to print the node logo
+function printNodeLogo {
+    echo -e "\033[32m"
+    echo "          
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+██████████████████████████████████████████████        ██████████████████████████████████████████████
+███████████████████████████████████████████              ███████████████████████████████████████████
+████████████████████████████████████████                    ████████████████████████████████████████
+█████████████████████████████████████                          █████████████████████████████████████
+█████████████████████████████████                                  █████████████████████████████████
+██████████████████████████████             █             █            ██████████████████████████████
+████████████████████████████           █████             ████           ████████████████████████████
+████████████████████████████          ██████             ██████         ████████████████████████████
+████████████████████████████          ██████             ██████          ███████████████████████████
+████████████████████████████          ███████            ██████          ███████████████████████████
+████████████████████████████          ██████████         ██████          ███████████████████████████
+████████████████████████████          █████████████      ██████          ███████████████████████████
+████████████████████████████             █████████████     ████          ███████████████████████████
+████████████████████████████          █     █████████████     █          ███████████████████████████
+████████████████████████████          █████     ████████████             ███████████████████████████
+████████████████████████████          ██████       ████████████          ███████████████████████████
+████████████████████████████          ██████          █████████          ███████████████████████████
+████████████████████████████          ██████             ██████          ███████████████████████████
+████████████████████████████          ██████             ██████          ███████████████████████████
+████████████████████████████          ██████             ██████         ████████████████████████████
+████████████████████████████            ████             ███            ████████████████████████████
+██████████████████████████████                                        ██████████████████████████████
+█████████████████████████████████                                  █████████████████████████████████
+█████████████████████████████████████                           ████████████████████████████████████
+████████████████████████████████████████                    ████████████████████████████████████████
+███████████████████████████████████████████              ███████████████████████████████████████████
+██████████████████████████████████████████████        ██████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+████████████████████████████████████████████████████████████████████████████████████████████████████
+Hazen Network Solutions 2025 All rights reserved."
+    echo -e "\033[0m"
+}
+
+# Show the node logo
+printNodeLogo
+
+# User confirmation to proceed
+echo -n "Type 'yes' to start the installation Lumera Testnet v1.0.1 and press Enter: "
+read user_input
+
+if [[ "$user_input" != "yes" ]]; then
+  echo "Installation cancelled."
+  exit 1
+fi
+
+# Function to print in green
+printGreen() {
+  echo -e "\033[32m$1\033[0m"
+}
+
+printGreen "Starting installation..."
+sleep 1
+
+printGreen "If there are any, clean up the previous installation files"
+
+sudo systemctl stop lumerad
+sudo systemctl disable lumerad
+sudo rm -rf /etc/systemd/system/lumerad.service
+sudo rm $(which lumerad)
+sudo rm -rf $HOME/.lumera
+sed -i "/LUMERA_/d" $HOME/.bash_profile
+
+# Update packages and install dependencies
+printGreen "1. Updating and installing dependencies..."
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install curl git wget htop tmux build-essential jq make lz4 gcc unzip -y
+
+# User inputs
+read -p "Enter your MONIKER: " MONIKER
+echo 'export MONIKER='$MONIKER
+read -p "Enter your PORT (2-digit): " PORT
+echo 'export PORT='$PORT
+
+# Setting environment variables
+echo "export MONIKER=$MONIKER" >> $HOME/.bash_profile
+echo "export LUMERA_CHAIN_ID=\"lumera-testnet-1\"" >> $HOME/.bash_profile
+echo "export LUMERA_PORT=$PORT" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+printLine
+echo -e "Moniker:        \e[1m\e[32m$MONIKER\e[0m"
+echo -e "Chain ID:       \e[1m\e[32m$LUMERA_CHAIN_ID\e[0m"
+echo -e "Node custom port:  \e[1m\e[32m$LUMERA_PORT\e[0m"
+printLine
+sleep 1
+
+# Install Go
+printGreen "2. Installing Go..." && sleep 1
+cd $HOME
+VER="1.23.4"
+wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$VER.linux-amd64.tar.gz"
+rm "go$VER.linux-amd64.tar.gz"
+[ ! -f ~/.bash_profile ] && touch ~/.bash_profile
+echo "export PATH=$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bash_profile
+source $HOME/.bash_profile
+[ ! -d ~/go/bin ] && mkdir -p ~/go/bin
+
+# Version check
+echo $(go version) && sleep 1
+
+# Download Prysm protocol binary
+printGreen "3. Downloading Lumera binary and setting up..." && sleep 1
+
+mkdir -p $HOME/.lumera/cosmovisor/genesis/bin
+wget -O $HOME/.lumera/cosmovisor/genesis/bin/lumerad https://server-9.hazennetworksolutions.com/lumerad
+chmod +x $HOME/.lumera/cosmovisor/genesis/bin/lumerad
+
+wget -O /usr/lib/libwasmvm.x86_64.so https://server-9.hazennetworksolutions.com/libwasmvm.x86_64.so
+
+sudo ln -s ~/.lumera/cosmovisor/genesis ~/.lumera/cosmovisor/current -f
+sudo ln -s ~/.lumera/cosmovisor/current/bin/lumerad /usr/local/bin/lumerad -f
+
+go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.6.0
+
+# Create service file
+sudo tee /etc/systemd/system/lumerad.service > /dev/null << EOF
+[Unit]
+Description=lumera node service
+After=network-online.target
+
+[Service]
+User=$USER
+ExecStart=$(which cosmovisor) run start --home $HOME/.lumera
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+Environment="DAEMON_HOME=${HOME}/.lumera"
+Environment="DAEMON_NAME=lumerad"
+Environment="UNSAFE_SKIP_BACKUP=true"
+Environment="PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:~/.atomone/cosmovisor/current/bin"
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Enable the service
+sudo systemctl daemon-reload
+sudo systemctl enable lumerad
+
+# Initialize the node
+printGreen "7. Initializing the node..."
+atomoned init $MONIKER --chain-id $LUMERA_CHAIN_ID
+sed -i \
+-e "s/chain-id = .*/chain-id = \"lumera-testnet-1\"/" \
+-e "s/keyring-backend = .*/keyring-backend = \"os\"/" \
+-e "s/node = .*/node = \"tcp:\/\/localhost:${LUMERA_PORT}657\"/" $HOME/.lumera/config/client.toml
+
+# Download genesis and addrbook files
+printGreen "8. Downloading genesis and addrbook..."
+wget -O $HOME/.lumera/config/genesis.json https://raw.githubusercontent.com/hazennetworksolutions/lumera/refs/heads/main/genesis.json
+wget -O $HOME/.lumera/config/addrbook.json  https://raw.githubusercontent.com/hazennetworksolutions/lumera/refs/heads/main/addrbook.json
+
+
+# Configure gas prices and ports
+printGreen "9. Configuring custom ports and gas prices..." && sleep 1
+sed -i -e "s|^minimum-gas-prices *=.*|minimum-gas-prices = \"0.025ulume\"|" $HOME/.lumera/config/app.toml
+sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.lumera/config/config.toml
+sed -i -e "s/^indexer *=.*/indexer = \"null\"/" $HOME/.lumera/config/config.toml
+
+sed -i.bak -e "s%:1317%:${LUMERA_PORT}317%g;
+s%:8080%:${LUMERA_PORT}080%g;
+s%:9090%:${LUMERA_PORT}090%g;
+s%:9091%:${LUMERA_PORT}091%g;
+s%:8545%:${LUMERA_PORT}545%g;
+s%:8546%:${LUMERA_PORT}546%g;
+s%:6065%:${LUMERA_PORT}065%g" $HOME/.lumera/config/app.toml
+
+# Configure P2P and ports
+sed -i.bak -e "s%:26658%:${LUMERA_PORT}658%g;
+s%:26657%:${LUMERA_PORT}657%g;
+s%:6060%:${LUMERA_PORT}060%g;
+s%:26656%:${LUMERA_PORT}656%g;
+s%^external_address = \"\"%external_address = \"$(wget -qO- eth0.me):${LUMERA_PORT}656\"%;
+s%:26660%:${LUMERA_PORT}660%g" $HOME/.lumera/config/config.toml
+
+# Set up seeds and peers
+printGreen "10. Setting up peers and seeds..." && sleep 1
+
+sed -i -e "s|^seeds *=.*|seeds = \"3f472746f46493309650e5a033076689996c8881@lumera-testnet.rpc.kjnodes.com:16959\"|" $HOME/.lumera/config/config.toml
+
+
+# Pruning Settings
+sed -i -e "s/^pruning *=.*/pruning = \"custom\"/" $HOME/.lumera/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"100\"/" $HOME/.lumera/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"19\"/" $HOME/.lumera/config/app.toml
+
+# Download the snapshot
+# printGreen "12. Downloading snapshot and starting node..." && sleep 1
+
+
+
+
+
+# Start the node
+printGreen "13. Starting the node..."
+sudo systemctl start lumerad
+
+# Check node status
+printGreen "14. Checking node status..."
+sudo journalctl -u lumerad -f -o cat
+
+# Verify if the node is running
+if systemctl is-active --quiet lumerad; then
+  echo "The node is running successfully! Logs can be found at /var/log/lumera_node_install.log"
+else
+  echo "The node failed to start. Logs can be found at /var/log/lumera_node_install.log"
+fi
