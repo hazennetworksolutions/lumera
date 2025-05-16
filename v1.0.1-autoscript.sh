8e2f19d88477d18e342fe2d1e5a498638f052722
hazennetworksolutions <161881320+hazennetworksolutions@users.noreply.github.com>
2025-05-16 22:53:53 +0300

    Update v1.0.1-autoscript.sh

--- v1.0.1-autoscript.sh
+++ v1.0.1-autoscript.sh
@@ -231,8 +231,10 @@ sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"19\"/" $HOME/.lumera/co
 # Download the snapshot
 # printGreen "12. Downloading snapshot and starting node..." && sleep 1
 
-
-
+cp $HOME/.lumera/data/priv_validator_state.json $HOME/.lumera/priv_validator_state.json.backup
+rm -rf $HOME/.lumera/data
+curl https://server-9.hazennetworksolutions.com/lumera-testnet-snapshot.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.lumera
+mv $HOME/.atomone/priv_validator_state.json.backup $HOME/.atomone/data/priv_validator_state.json
 
 
 # Start the node
